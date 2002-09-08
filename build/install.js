2ab0e6358bed404d19601d0a9e69e51e9bd483cc
saravn <saravn>
2002-09-08 06:46:01 +0000

    Upped version to 0.70.0 to reflect l10n work and incompatibility of 0.6x lang packs

--- build/install.js
+++ build/install.js
@@ -2,9 +2,9 @@
 
 var err;
 
-err = initInstall("Enigmime v0.66.0", // name for install UI
+err = initInstall("Enigmime v0.70.0", // name for install UI
                   "/enigmime",         // registered name
-                  "0.66.0.0");        // package version
+                  "0.70.0.0");        // package version
 
 logComment("initInstall: " + err);
 
