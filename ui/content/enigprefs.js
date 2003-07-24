3460387caf36739dd592a68b8486c2050110e302
patrick <patrick>
2003-07-24 12:05:12 +0000

    default values for enigmail Account Manager

---
+++ ui/content/enigprefs.js
@@ -0,0 +1,15 @@
+/*
+   Add any default pref values we want for the enigmail per-account
+   settings
+*/
+
+pref("mail.identity.default.enablePgp",false);
+pref("mail.identity.default.pgpkeyId",  "");
+pref("mail.identity.default.pgpKeyMode", 0);
+pref("mail.identity.default.noPgpPassphrase", false);
+pref("mail.identity.default.pgpAlwaysSign", false);
+pref("mail.identity.default.defaultEncryptionOption", 0);
+
+
+
+
