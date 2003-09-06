55c1f08d3c8513939533f5e50bb7ca16fcffa45b
patrick <patrick>
2003-09-06 17:37:25 +0000

    added print preview support from context menu fixed printing headers if error from Mozilla thrown

--- ui/content/enigmailMessengerOverlay.js
+++ ui/content/enigmailMessengerOverlay.js
@@ -40,8 +40,8 @@ function enigMessengerStartup() {
 
   // Override print command
   var printElementIds = ["cmd_print", "cmd_printpreview", "key_print", "button-print",
-                         "threadPaneContext-print",
-                         "messagePaneContext-print"];
+                         "threadPaneContext-print", "threadPaneContext-printpreview",
+                         "messagePaneContext-print", "messagePaneContext-printpreview"];
 
   EnigOverrideAttribute( printElementIds, "oncommand",
                          "enigMsgPrint('", "');");
@@ -917,7 +917,14 @@ function enigGetDecryptedMessage(contentType, includeHeaders) {
               contentData += headerName + ": " + msgHdr[headerName] + "\r\n";
           }
         }
-      } catch (ex) {}
+      } catch (ex) {
+        // the above seems to fail every now and then
+        // so, here is the fallback
+        for (headerName in headerList) {
+          headerValue = headerList[headerName];
+          contentData += headerName + ": " + headerValue + "\r\n";
+        }
+      }
     }
 
     contentData += "Content-Type: text/plain";
@@ -1003,16 +1010,13 @@ function enigMsgForward(elementId, event) {
   }
 }
 
-function enigMsgDefaultPrint(contextMenu, elementId) {
-  DEBUG_LOG("enigmailMessengerOverlay.js: enigMsgDefaultPrint: "+contextMenu+"\n");
+function enigMsgDefaultPrint(elementId) {
+  DEBUG_LOG("enigmailMessengerOverlay.js: enigMsgDefaultPrint: "+elementId+"\n");
 
   // Reset mail.show_headers pref to "original" value
   EnigShowHeadersAll(false);
 
-  if (contextMenu)
-    PrintEnginePrint();
-  else
-    goDoCommand(elementId == "cmd_printpreview" ? "cmd_printpreview" : "cmd_print");
+  goDoCommand(elementId.indexOf("printpreview")>=0 ? "cmd_printpreview" : "cmd_print");
 }
 
 function enigMsgPrint(elementId) {
@@ -1021,21 +1025,21 @@ function enigMsgPrint(elementId) {
   var contextMenu = (elementId.search("Context") > -1);
 
   if (!gEnigDecryptedMessage)
-    enigMsgDefaultPrint(contextMenu, elementId);
+    enigMsgDefaultPrint(elementId);
 
   var mailNewsUrl = enigGetCurrentMsgUrl();
 
   if (!mailNewsUrl)
-    enigMsgDefaultPrint(contextMenu, elementId);
+    enigMsgDefaultPrint(elementId);
 
   if (gEnigDecryptedMessage.url != mailNewsUrl.spec) {
     gEnigDecryptedMessage = null;
-    enigMsgDefaultPrint(contextMenu);
+    enigMsgDefaultPrint(elementId);
   }
 
   var enigmailSvc = GetEnigmailSvc();
   if (!enigmailSvc)
-    enigMsgDefaultPrint(contextMenu, elementId);
+    enigMsgDefaultPrint(elementId);
 
   // Note: Trying to print text/html content does not seem to work with
   //       non-ASCII chars
@@ -1057,7 +1061,7 @@ function enigMsgPrint(elementId) {
     gPrintSettings = GetPrintSettings();
   }
 
-  var printPreview = (elementId == "cmd_printpreview");
+  var printPreview = (elementId.indexOf("printpreview")>=0);
   var printEngineWindow = window.openDialog("chrome://messenger/content/msgPrintEngine.xul",
                                         "",
                                         "chrome,dialog=no,all,centerscreen",
@@ -1102,6 +1106,8 @@ function enigMessageSave() {
 
   var textContent = enigGetDecryptedMessage("text/plain", true);
 
+  EnigAlert(textContent);
+  
   if (!EnigWriteFileContents(saveFile.path, textContent, null)) {
     EnigAlert("Error in saving to file "+saveFile.path);
     return;
