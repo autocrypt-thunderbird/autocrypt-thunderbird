6b52ea9f9c164ce07fb13b11de2cd2961bee7931
patrick <patrick>
2003-09-08 18:59:40 +0000

    fixed garbled version string

--- build/install.js
+++ build/install.js
@@ -2,7 +2,7 @@
 
 var err;
 
-err = initInstall("Enigmime v0.81.6, // name for install UI
+err = initInstall("Enigmime v0.81.6", // name for install UI
                   "/enigmime",         // registered name
                   "0.81.6.0");        // package version
 
