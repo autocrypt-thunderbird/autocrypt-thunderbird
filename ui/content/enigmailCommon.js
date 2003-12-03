b92682aa745f2b4305b83555671579ab45b6e100
patrick <patrick>
2003-12-03 08:30:30 +0000

    enigmail v0.82.4 release

--- ui/content/enigmailCommon.js
+++ ui/content/enigmailCommon.js
@@ -1,8 +1,8 @@
 // enigmailCommon.js: shared JS functions for Enigmail
 
 // This Enigmail version and compatible Enigmime version
-var gEnigmailVersion = "0.82.3.0";
-var gEnigmimeVersion = "0.82.3.0";
+var gEnigmailVersion = "0.82.4.0";
+var gEnigmimeVersion = "0.82.4.0";
 
 // Maximum size of message directly processed by Enigmail
 const ENIG_MSG_BUFFER_SIZE = 96000;
--- ui/enigmail-en-US.spec
+++ ui/enigmail-en-US.spec
@@ -1,7 +1,7 @@
 Name:      mozilla-enigmail-en-US
-Version:   0.82.3
+Version:   0.82.4
 Release:   1
-Requires:  mozilla-enigmail = 0.82.3
+Requires:  mozilla-enigmail = 0.82.4
 Summary:   Language pack for Enigmail (en-US)
 Copyright: Mozilla Public License 1.1/GPL
 Group:     Applications/Internet
