678c8ea6675cf14f9b20d27026a77aeaeaf8c3ac
patrick <patrick>
2004-02-04 12:52:11 +0000

    version 0.83.2

--- build/enigmime.spec
+++ build/enigmime.spec
@@ -1,5 +1,5 @@
 Name:      mozilla-enigmime
-Version:   0.83.1
+Version:   0.83.2
 Release:   1
 Requires:  mozilla = 1.0.1, mozilla-mail = 1.0.1, mozilla-psm = 1.0.1
 Summary:   MIME and Inter-Process Communication for Enigmail/Mozilla
--- package/install.js
+++ package/install.js
@@ -1,7 +1,7 @@
 // Install script for Enigmail
 
 var err;
-const APP_VERSION="0.83.1";
+const APP_VERSION="0.83.2";
 
 err = initInstall("Enigmail v"+APP_VERSION,  // name for install UI
                   "/enigmail",         // registered name
