cd4563f102342b3812fc7ae0f941d118f5bfbbe3
patrick <patrick>
2004-02-13 16:11:00 +0000

    v0.83.3

--- ui/content/enigmailCommon.js
+++ ui/content/enigmailCommon.js
@@ -34,8 +34,8 @@ GPL.
 // enigmailCommon.js: shared JS functions for Enigmail
 
 // This Enigmail version and compatible Enigmime version
-var gEnigmailVersion = "0.83.2.0";
-var gEnigmimeVersion = "0.83.2.0";
+var gEnigmailVersion = "0.83.3.0";
+var gEnigmimeVersion = "0.83.3.0";
 
 // Maximum size of message directly processed by Enigmail
 const ENIG_MSG_BUFFER_SIZE = 96000;
