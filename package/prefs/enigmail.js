eb3dcd1862129f8804b61fd8c64394489c02477e
patrick <patrick>
2004-03-02 09:21:06 +0000

    enigmail default preferences

---
+++ package/prefs/enigmail.js
@@ -0,0 +1,56 @@
+/*
+   Default pref values for Enigmail
+*/
+
+pref("extensions.enigmail.configuredVersion","");
+pref("extensions.enigmail.disableSMIMEui",false);
+pref("extensions.enigmail.agentPath","");
+pref("extensions.enigmail.alwaysTrustSend",true);
+pref("extensions.enigmail.autoCrypto",false);
+pref("extensions.enigmail.autoDecrypt",true);
+pref("extensions.enigmail.captureWebMail",false);
+pref("extensions.enigmail.composeHtmlAlertCount",3);
+pref("extensions.enigmail.confirmBeforeSend",false);
+pref("extensions.enigmail.disableSMIMEui",false);
+pref("extensions.enigmail.displaySignWarn",true);
+pref("extensions.enigmail.displayPartiallySigned",true);
+pref("extensions.enigmail.doubleDashSeparator",true);
+pref("extensions.enigmail.encryptAttachments",1);
+pref("extensions.enigmail.encryptToSelf",true);
+pref("extensions.enigmail.gpgVersionWarnCount",1);
+pref("extensions.enigmail.handleDoubleClick",false);
+pref("extensions.enigmail.hushMailSupport",false);
+pref("extensions.enigmail.initAlertCount",2);
+pref("extensions.enigmail.inlineAttachAsciiArmor",false);
+pref("extensions.enigmail.inlineAttachExt",".pgp");
+pref("extensions.enigmail.logDirectory","");
+pref("extensions.enigmail.keepSettingsForReply",true);
+pref("extensions.enigmail.keyserver","random.sks.keyserver.penguin.de, pgp.dtype.org, keyserver.kjsl.com, ldap,//certserver.pgp.com");
+pref("extensions.enigmail.maxIdleMinutes",5);
+pref("extensions.enigmail.mimeHashAlgorithm",1);
+pref("extensions.enigmail.noPassphrase",false);
+pref("extensions.enigmail.parseAllHeaders",true);
+pref("extensions.enigmail.quotedPrintableWarn",0);
+pref("extensions.enigmail.respectHttpProxy",true);
+pref("extensions.enigmail.recipientsSelectionOption",1);
+pref("extensions.enigmail.saveEncrypted",0);
+pref("extensions.enigmail.show_headers",1);
+pref("extensions.enigmail.supportMultiPass",false);
+pref("extensions.enigmail.useDefaultComment",false);
+pref("extensions.enigmail.useGpgAgent",false);
+pref("extensions.enigmail.usePGPMimeOption",1);
+pref("extensions.enigmail.wrapHtmlBeforeSend",true);
+
+/*
+   Default pref values for the enigmail per-account
+   settings
+*/
+
+pref("mail.identity.default.enablePgp",false);
+pref("mail.identity.default.pgpkeyId",  "");
+pref("mail.identity.default.pgpKeyMode", 0);
+pref("mail.identity.default.pgpSignPlain", false);
+pref("mail.identity.default.pgpSignEncrypted", false);
+pref("mail.identity.default.defaultEncryptionPolicy", 0);
+
+
