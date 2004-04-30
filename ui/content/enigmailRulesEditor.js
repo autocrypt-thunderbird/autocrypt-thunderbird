58ca2a35162efd57c1cabdb11a2d04040314e251
patrick <patrick>
2004-04-30 20:59:09 +0000

    new PGP Rules frontend

---
+++ ui/content/enigmailRulesEditor.js
@@ -0,0 +1,228 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "MPL"); you may not use this file
+ * except in compliance with the MPL. You may obtain a copy of
+ * the MPL at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the MPL is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the MPL for the specific language governing
+ * rights and limitations under the MPL.
+ *
+ * The Original Code is Enigmail.
+ *
+ * The Initial Developer of this code is Patrick Brunschwig.
+ * Portions created by Patrick Brunschwig <patrick.brunschwig@gmx.net>
+ * are Copyright (C) 2003 Patrick Brunschwig.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the
+ * terms of the GNU General Public License (the "GPL") or the GNU 
+ * Lesser General Public License (the "LGPL"), in which case
+ * the provisions of the GPL or the LGPL are applicable instead of
+ * those above. If you wish to allow use of your version of this
+ * file only under the terms of the GPL or the LGPL and not to allow
+ * others to use your version of this file under the MPL, indicate
+ * your decision by deleting the provisions above and replace them
+ * with the notice and other provisions required by the GPL or the 
+ * LGPL respectively.
+ * If you do not delete the provisions above, a recipient
+ * may use your version of this file under either the MPL, the
+ * GPL or the LGPL.
+ */
+
+// Uses: chrome://enigmail/content/enigmailCommon.js
+
+// Initialize enigmailCommon
+EnigInitCommon("enigmailRulesEditor");
+
+const INPUT=0;
+const RESULT=1;
+ 
+function enigmailDlgOnLoad() {
+  var enigmailSvc = GetEnigmailSvc();
+  if (!enigmailSvc)
+    return false;
+
+  var rulesListObj=new Object;
+  if (enigmailSvc.getRulesData(rulesListObj)) {
+    var treeChildren=document.getElementById("rulesTreeChildren");
+    var rulesList=rulesListObj.value;
+        if (rulesList.firstChild.nodeName=="parsererror") {
+      EnigAlert("Invalid enigmail.xml file:\n"+ rulesList.firstChild.textContent);
+      return 0;
+    }
+    DEBUG_LOG("enigmailRulesEditor.js: dlgOnLoad: keys loaded\n");
+    var node=rulesList.firstChild.firstChild;
+    while (node) {
+      if (node.tagName=="pgpRule") {
+        //try {
+          var userObj={ 
+                email: node.getAttribute("email"),
+                keyId: node.getAttribute("keyId"),
+                sign: node.getAttribute("sign"),
+                encrypt: node.getAttribute("encrypt"),
+                pgpMime: node.getAttribute("pgpMime")
+              };
+          var treeItem=document.createElement("treeitem");
+          createRow(treeItem, userObj);
+          treeChildren.appendChild(treeItem);
+        //}
+        //catch (ex) {}
+      }
+      node = node.nextSibling;
+    }
+  }
+  var rulesTree=document.getElementById("rulesTree");
+}
+
+function enigmailDlgOnAccept() {
+  DEBUG_LOG("enigmailRulesEditor.js: dlgOnAccept:\n");
+  var enigmailSvc = GetEnigmailSvc();
+  if (!enigmailSvc)
+    return false;
+  enigmailSvc.clearRules();
+  
+  var node=document.getElementById("rulesTreeChildren").firstChild;
+  while(node) {
+    enigmailSvc.addRule(node.getAttribute("email"),
+                        node.getAttribute("keyId"),
+                        node.getAttribute("sign"),
+                        node.getAttribute("encrypt"),
+                        node.getAttribute("pgpMime"));
+    node = node.nextSibling;
+  }
+  enigmailSvc.saveRulesFile();
+  
+  return true;
+}
+
+function createCol(value, label, treeItem, translate) {
+  var column=document.createElement("treecell");
+  column.setAttribute("id", value);
+  treeItem.setAttribute(value, label);
+  if (translate) {
+    switch (Number(label)) {
+    case 0: 
+      label=EnigGetString("never");
+      break;
+    case 1: 
+      label=EnigGetString("possible");
+      break;
+    case 2: 
+      label=EnigGetString("always");
+      break;
+    }
+  }
+  column.setAttribute("label", label);
+  return column;
+}
+
+function createRow(treeItem, userObj) {
+  var email=createCol("email", userObj.email, treeItem, false);
+  var keyId=createCol("keyId", userObj.keyId, treeItem, false);
+  var sign=createCol("sign", userObj.sign, treeItem, true);
+  var encrypt=createCol("encrypt", userObj.encrypt, treeItem, true);
+  var pgpMime=createCol("pgpMime", userObj.pgpMime, treeItem, true);
+  var treeRow=document.createElement("treerow");
+  treeRow.appendChild(email);
+  treeRow.appendChild(keyId);
+  treeRow.appendChild(sign);
+  treeRow.appendChild(encrypt);
+  treeRow.appendChild(pgpMime);
+  
+  if (treeItem.firstChild) {
+    treeItem.replaceChild(treeRow, treeItem.firstChild);
+  }
+  else {
+    treeItem.appendChild(treeRow);
+  }
+}
+
+function getCurrentNode() {
+  var rulesTree=document.getElementById("rulesTree");
+  if (rulesTree.currentIndex <0) return null;
+  var node=document.getElementById("rulesTreeChildren").firstChild;
+  for (var i=0; i<rulesTree.currentIndex && node; i++) {
+    node = node.nextSibling;
+  }
+  return node;
+}
+
+
+function enigDoEdit() {
+  var node=getCurrentNode()
+  if (node) {
+    var inputObj  = new Object;
+    var resultObj = new Object;
+    inputObj.options="nosave";
+    inputObj.toAddress = node.getAttribute("email");
+    inputObj.keyId     = node.getAttribute("keyId").split(/[ ,]+/);
+    inputObj.sign      = Number(node.getAttribute("sign"));
+    inputObj.encrypt   = Number(node.getAttribute("encrypt"));
+    inputObj.pgpmime   = Number(node.getAttribute("pgpMime"));
+    
+    window.openDialog("chrome://enigmail/content/enigmailSingleRcptSettings.xul","", "dialog,modal,centerscreen,resizable", inputObj, resultObj);
+    if (resultObj.cancelled==false) {
+      createRow(node, resultObj);
+    }
+  }
+}
+
+function enigDoAdd() {
+  var inputObj  = new Object;
+  var resultObj = new Object;
+  inputObj.options="nosave";
+  inputObj.toAddress = "{}";
+  
+  window.openDialog("chrome://enigmail/content/enigmailSingleRcptSettings.xul","", "dialog,modal,centerscreen,resizable", inputObj, resultObj);
+  if (resultObj.cancelled==false) {
+    var treeItem=document.createElement("treeitem");
+    createRow(treeItem, resultObj);
+    var treeChildren=document.getElementById("rulesTreeChildren");
+    treeChildren.appendChild(treeItem);
+  }
+}
+
+function enigDoDelete() {
+  var node=getCurrentNode();
+  if (node) {
+    if (EnigConfirm(EnigGetString("deleteRule"))) {
+      var treeChildren=document.getElementById("rulesTreeChildren");
+      treeChildren.removeChild(node);
+    }
+  }
+}
+
+function enigDoMoveUp() {
+  var node=getCurrentNode();
+  if (! node) return;
+  var prev=node.previousSibling;
+  if (prev) {
+    var rulesTree=document.getElementById("rulesTree");
+    var currentIndex = rulesTree.currentIndex;
+    var treeChildren=document.getElementById("rulesTreeChildren");
+    var newNode=node.cloneNode(true);
+    treeChildren.removeChild(node);
+    treeChildren.insertBefore(newNode, prev);
+    rulesTree.currentIndex = -1;
+    rulesTree.currentIndex = currentIndex - 1;
+  }
+}
+
+function enigDoMoveDown() {
+  var node=getCurrentNode();
+  if (! node) return;
+  var nextNode=node.nextSibling;
+  if (nextNode) {
+    var rulesTree=document.getElementById("rulesTree");
+    var currentIndex = rulesTree.currentIndex;
+    var treeChildren=document.getElementById("rulesTreeChildren");
+    var newNode=nextNode.cloneNode(true);
+    treeChildren.removeChild(nextNode);
+    treeChildren.insertBefore(newNode, node);
+    rulesTree.currentIndex = currentIndex + 1;
+  }
+}
---
+++ ui/content/enigmailSingleRcptSettings.js
@@ -0,0 +1,209 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "MPL"); you may not use this file
+ * except in compliance with the MPL. You may obtain a copy of
+ * the MPL at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the MPL is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the MPL for the specific language governing
+ * rights and limitations under the MPL.
+ *
+ * The Original Code is Enigmail.
+ *
+ * The Initial Developer of this code is Patrick Brunschwig.
+ * Portions created by Patrick Brunschwig <patrick.brunschwig@gmx.net>
+ * are Copyright (C) 2003 Patrick Brunschwig.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the
+ * terms of the GNU General Public License (the "GPL") or the GNU 
+ * Lesser General Public License (the "LGPL"), in which case
+ * the provisions of the GPL or the LGPL are applicable instead of
+ * those above. If you wish to allow use of your version of this
+ * file only under the terms of the GPL or the LGPL and not to allow
+ * others to use your version of this file under the MPL, indicate
+ * your decision by deleting the provisions above and replace them
+ * with the notice and other provisions required by the GPL or the 
+ * LGPL respectively.
+ * If you do not delete the provisions above, a recipient
+ * may use your version of this file under either the MPL, the
+ * GPL or the LGPL.
+ */
+
+// Uses: chrome://enigmail/content/enigmailCommon.js
+
+// Initialize enigmailCommon
+EnigInitCommon("enigmailSingleRcptSettings");
+
+const INPUT=0;
+const RESULT=1;
+ 
+function enigmailDlgOnLoad() {
+  var ruleEmail=document.getElementById("ruleEmail");
+  if (window.arguments[INPUT].toAddress.indexOf("{")==0) {
+    document.getElementById("matchBegin").checked=true;
+  }
+  if (window.arguments[INPUT].toAddress.search(/}$/)>=0) {
+    document.getElementById("matchEnd").checked=true;
+  }
+  
+  ruleEmail.value = window.arguments[INPUT].toAddress.replace(/[{}]/g, "");
+  window.arguments[RESULT].cancelled=true;
+  if (typeof(window.arguments[INPUT].keyId)=="object") {
+    enigSetKeys(window.arguments[INPUT].keyId);
+  }
+  else {
+    enigSetKeys("");
+  }
+  
+  if (typeof(window.arguments[INPUT].sign)=="number") {
+    document.getElementById("sign").selectedIndex=window.arguments[INPUT].sign;
+  }
+  else {
+    document.getElementById("sign").selectedIndex=1;
+  }
+  if (typeof(window.arguments[INPUT].encrypt)=="number") {
+    document.getElementById("encrypt").selectedIndex=window.arguments[INPUT].encrypt;
+  }
+  else {
+    document.getElementById("encrypt").selectedIndex=1;
+  }
+  if (typeof(window.arguments[INPUT].pgpmime)=="number") {
+    document.getElementById("pgpmime").selectedIndex=window.arguments[INPUT].pgpmime;
+  }
+  else {
+    document.getElementById("pgpmime").selectedIndex=1;
+  }
+}
+ 
+function enigmailDlgOnAccept() {
+  var enigmailSvc = GetEnigmailSvc();
+  if (!enigmailSvc)
+    return false;
+
+  var keyList="";
+  var ruleEmail  = document.getElementById("ruleEmail");
+  var matchBegin = document.getElementById("matchBegin").checked;
+  var matchEnd   = document.getElementById("matchEnd").checked;
+  
+  if (ruleEmail.value.length==0) {
+    EnigAlert(EnigGetString("noEmptyRule"));
+    return false;
+  }
+  if (ruleEmail.value.search(/[\<\>]/)>=0) {
+    EnigAlert(EnigGetString("invalidAddress"));
+    return false;
+  }
+  var encryptionList=document.getElementById("encryptionList");
+  for (var i=0; i<encryptionList.getRowCount(); i++) {
+    var item=encryptionList.getItemAtIndex(i);
+    var valueLabel=item.getAttribute("value");
+    if (valueLabel.length>0) {
+      keyList+=", "+valueLabel;
+    }
+  }
+  var email="";
+  var mailAddrs=ruleEmail.value.split(/[ ,]+/);
+  for (i=0; i<mailAddrs.length; i++) {
+    email += (matchBegin ? " {" : " ") + mailAddrs[i] + (matchEnd ? "}" : "");
+  }
+  window.arguments[RESULT].email   = email.substr(1);
+  window.arguments[RESULT].keyId   = keyList.substr(2);
+  window.arguments[RESULT].sign    = document.getElementById("sign").value;
+  window.arguments[RESULT].encrypt = document.getElementById("encrypt").value;
+  window.arguments[RESULT].pgpMime = document.getElementById("pgpmime").value;
+  if (keyList == "" && (window.arguments[RESULT].encrypt>0)) {
+    if (!EnigConfirm(EnigGetString("noEncryption", window.arguments[INPUT].toAddress, window.arguments[INPUT].toAddress))) {
+      return false;
+    }
+    window.arguments[RESULT].encrypt=0;
+  }
+
+  window.arguments[RESULT].cancelled=false;
+  if (window.arguments[INPUT].options.indexOf("nosave")<0) {
+    enigmailSvc.addRule(window.arguments[RESULT].email,
+                        window.arguments[RESULT].keyId,
+                        window.arguments[RESULT].sign,
+                        window.arguments[RESULT].encrypt,
+                        window.arguments[RESULT].pgpMime);
+   enigmailSvc.saveRulesFile();
+  }
+  return true;
+}
+
+function enigmailDlgKeySelection() {
+  DEBUG_LOG("enigmailMsgComposeHelper.js: enigmailDlgKeySelection: \n");
+
+  var enigmailSvc = GetEnigmailSvc();
+  if (!enigmailSvc)
+    return false;
+
+  var resultObj = new Object();
+  var inputObj = new Object();
+  inputObj.dialogHeader = "";
+  inputObj.forUser = document.getElementById("ruleEmail").value.replace(/[ ,]+/g, ", ");
+  inputObj.toAddr = inputObj.forUser;
+  inputObj.toKeys = "";
+  var encryptionList=document.getElementById("encryptionList");
+  encryptionList.clearSelection();
+  for (var i=0; i<encryptionList.getRowCount(); i++) {
+    var item=encryptionList.getItemAtIndex(i);
+    var valueLabel=item.getAttribute("value");
+    if (valueLabel.length>0) {
+      inputObj.toKeys += valueLabel+",";
+    }
+  }
+  
+  inputObj.options = "multisel,forUser";
+  var userIdValue="";
+
+  window.openDialog("chrome://enigmail/content/enigmailUserSelection.xul","", "dialog,modal,centerscreen", inputObj, resultObj);
+  try {
+    if (resultObj.cancelled) return;
+  } catch (ex) {
+    // cancel pressed -> do nothing
+    return;
+  }
+  enigSetKeys(resultObj.userList);
+}
+
+function enigSetKeys(keyList) {
+  var encryptionList=document.getElementById("encryptionList");
+  while (encryptionList.getRowCount()>0) {
+    encryptionList.removeItemAt(0);
+  }
+  if ((keyList.length==0) || (keyList.length==1 && keyList[0].length==0)) {
+    encryptionList.appendItem(EnigGetString("noKeyToUse"),"");
+  }
+  else {
+    var enigmailSvc = GetEnigmailSvc();
+    if (!enigmailSvc)
+      return false;
+      
+    var exitCodeObj= new Object;
+    var statusFlagsObj = new Object;
+    var errorMsgObj = new Object;
+    var userListTxt = enigmailSvc.getUserIdList(false,
+                                               false,
+                                               exitCodeObj,
+                                               statusFlagsObj,
+                                               errorMsgObj);
+    if (exitCodeObj.value != 0) {
+      EnigAlert(errorMsgObj.value);
+      return;
+    }
+
+    for (i=0; i<keyList.length; i++) {
+      var keyId=keyList[i].substring(2);
+      var keyStart=userListTxt.indexOf(":"+keyId+":");
+      var keyEnd=userListTxt.substring(keyStart).indexOf("\n");
+      var userDescList=userListTxt.substr(keyStart,keyEnd).split(/:/);
+     
+      encryptionList.appendItem("0x"+keyList[i].substr(10,8)+" ("+userDescList[6]+")",
+                                keyList[i]);
+    }
+  }
+}
