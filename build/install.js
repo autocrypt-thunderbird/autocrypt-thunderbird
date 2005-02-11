001264d7e04481644130c678b7a7a345b77f8c58
patrick <patrick>
2005-02-11 16:04:50 +0000

    v0.90.1 release

--- build/install.js
+++ build/install.js
@@ -2,9 +2,9 @@
 
 var err;
 
-err = initInstall("Enigmime v0.90.0", // name for install UI
+err = initInstall("Enigmime v0.90.1", // name for install UI
                   "/enigmime",         // registered name
-                  "0.90.0.0");        // package version
+                  "0.90.1.0");        // package version
 
 logComment("initInstall: " + err);
 
