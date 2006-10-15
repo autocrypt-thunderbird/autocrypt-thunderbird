d5be70269d95c6785344ce52036018b5931aab92
patrick <patrick>
2006-10-15 14:31:35 +0000

    removed obsolete comments

--- ipc/src/nsIPCBuffer.h
+++ ipc/src/nsIPCBuffer.h
@@ -97,7 +97,6 @@ protected:
     IPCFileDesc*                        mPipeRead;
 
     nsCOMPtr<nsILocalFile>              mTempFile;
-    //nsIFileSpec*                      mTempFileSpec;
     nsOutputFileStream*                 mTempOutStream;
     nsInputFileStream*                  mTempInStream;
 
