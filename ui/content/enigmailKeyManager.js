8b6e69fc4ccd6cd7e0d81950d82f175ea09e307f
patrick <patrick>
2009-12-14 12:13:52 +0000

    key download: close progress dialog before displaying result

--- ui/content/enigmailKeyManager.js
+++ ui/content/enigmailKeyManager.js
@@ -943,14 +943,18 @@ function enigmailRefreshAllKeys() {
   if (doIt) enigmailKeyServerAcess(nsIEnigmail.REFRESH_KEY, enigmailReceiveKeyCb);
 }
 
+function displayResult(msgText, refresh) {
+  EnigLongAlert(msgText);
+  if (refresh) enigmailRefreshKeys();
+}
+
 function enigmailReceiveKeyCb(exitCode, errorMsg, msgBox) {
   if (msgBox) {
     if (exitCode==0) {
-      EnigLongAlert(EnigGetString("receiveKeysOk") + "\n"+ errorMsg);
-      enigmailRefreshKeys();
+      window.setTimeout(displayResult, 100, EnigGetString("receiveKeysOk") + "\n"+ errorMsg, true);
     }
     else {
-      EnigLongAlert(EnigGetString("receiveKeysFailed")+"\n"+errorMsg);
+      window.setTimeout(displayResult, 100, EnigGetString("receiveKeysFailed")+"\n"+errorMsg, false);
     }
   }
   else {
