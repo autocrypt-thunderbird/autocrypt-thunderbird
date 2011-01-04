cb35c2d9a7674f2a71ba4b6628e2109f925c9d17
patrick <patrick>
2011-01-04 15:39:20 +0000

    changed license removed references to enigmailSelectPanel.* and enigmailPanel.*

--- package/enigmail.js
+++ package/enigmail.js
@@ -1,4 +1,6 @@
-/*
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
  * The contents of this file are subject to the Mozilla Public
  * License Version 1.1 (the "MPL"); you may not use this file
  * except in compliance with the MPL. You may obtain a copy of
@@ -16,20 +18,21 @@
  * Copyright (C) 2001 Ramalingam Saravanan. All Rights Reserved.
  *
  * Contributor(s):
- * Patrick Brunschwig <patrick.brunschwig@gmx.net>
+ * Patrick Brunschwig <patrick@mozilla-enigmail.org>
  *
- * Alternatively, the contents of this file may be used under the
- * terms of the GNU General Public License (the "GPL"), in which case
- * the provisions of the GPL are applicable instead of
- * those above. If you wish to allow use of your version of this
- * file only under the terms of the GPL and not to allow
- * others to use your version of this file under the MPL, indicate
- * your decision by deleting the provisions above and replace them
- * with the notice and other provisions required by the GPL.
- * If you do not delete the provisions above, a recipient
- * may use your version of this file under either the MPL or the
- * GPL.
- */
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ * ***** END LICENSE BLOCK ***** */
+
 
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 
@@ -201,8 +204,6 @@ const ENC_TYPE_ATTACH_BINARY = 1;
 const ENC_TYPE_ATTACH_ASCII = 2;
 
 const DUMMY_AGENT_INFO = "none";
-const ENIGMAIL_PANEL_URL = "chrome://enigmail/content/enigmailPanel.xul";
-
 
 var gMimeHashAlgorithms = [null, "sha1", "ripemd160", "sha256", "sha384", "sha512", "sha224"];
 
@@ -3711,50 +3712,6 @@ Enigmail.prototype = {
 
     return (delete this._messageIdList[messageId]);
   },
-
-
-  selectPanel: function (url) {
-    Ec.WRITE_LOG("enigmail.js: Enigmail.selectPanel: "+url+"\n");
-
-    var wm = Components.classes[WMEDIATOR_CONTRACTID].getService(Components.interfaces.nsIWindowMediator);
-    var navWindowList = wm.getEnumerator("navigator:browser");
-
-    var retval = false;
-    while (navWindowList.hasMoreElements()) {
-
-      var navWindow =navWindowList.getNext();
-      Ec.DEBUG_LOG("enigmail.js: navWindow="+navWindow+"\n");
-
-      var href = navWindow._content.location.href;
-      Ec.DEBUG_LOG("enigmail.js: href="+href+"\n");
-
-      if (href.toLowerCase().indexOf(url.toLowerCase()) != 0)
-        continue;
-
-      var enigmailPanel = navWindow.document.getElementById("urn:sidebar:3rdparty-panel:"+ENIGMAIL_PANEL_URL);
-      Ec.DEBUG_LOG("enigmail.js: panel="+enigmailPanel+"\n");
-
-      if (!enigmailPanel) {
-        // Add panel
-        enigmailAddPanel();
-
-        enigmailPanel = navWindow.document.getElementById("urn:sidebar:3rdparty-panel:"+ENIGMAIL_PANEL_URL);
-        Ec.DEBUG_LOG("enigmail.js: panel="+enigmailPanel+"\n");
-
-        if (!enigmailPanel) {
-          Ec.DEBUG_LOG("enigmail.js: Added panel not found in document!\n");
-          return false;
-        }
-      }
-
-      navWindow.SidebarSelectPanel(enigmailPanel, true, true);
-      retval = true;
-    }
-
-    return retval;
-  },
-
-
 
   invalidateUserIdList: function () {
     // clean the userIdList to force reloading the list at next usage
