de18f476eac993436b1025a815dc884a3deec84f
Patrick Brunschwig <patrick@enigmail.net>
2014-12-29 16:21:58 +0100

    added check to ensure that the filter action "Decrypt Permanently" cannot be used together with "Move" and "Copy" filter actions

---
+++ ui/content/enigmailFilterEditorOverlay.js
@@ -0,0 +1,88 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "MPL"); you may not use this file
+ * except in compliance with the MPL. You may obtain a copy of
+ * the MPL at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the MPL is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the MPL for the specific language governing
+ * rights and limitations under the MPL.
+ *
+ * The Original Code is Enigmail.
+ *
+ * The Initial Developer of the Original Code is Patrick Brunschwig.
+ * Portions created by Patrick Brunschwig <patrick@enigmail.net> are
+ * Copyright (C) 2014 Patrick Brunschwig. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ * ***** END LICENSE BLOCK ***** */
+
+// Uses: chrome://enigmail/content/enigmailCommon.js
+
+Components.utils.import("resource://enigmail/enigmailCommon.jsm");
+
+var enigmail_origCheckActionsReorder = checkActionsReorder;
+
+checkActionsReorder = function()
+{
+  enigmail_origCheckActionsReorder();
+  EnigmailCommon.setTimeout(EnigmailFilterEditor.checkMoveAction.bind(EnigmailFilterEditor), 0);
+}
+
+var EnigmailFilterEditor = {
+  checkMoveAction: function() {
+    let dlg = document.getElementById("FilterEditor");
+    let acceptButton = dlg.getButton("accept");
+    let forbidden = -1;
+    let hasCopyAction = -1;
+    let hasMoveAction = -1;
+
+    const nsMsgFilterAction = Components.interfaces.nsMsgFilterAction;
+
+    for (let i = 0; i < gActionListOrdered.length; i++) {
+      let action = gActionListOrdered.queryElementAt(i, Components.interfaces.nsIMsgRuleAction);
+      if (action.customId == "enigmail@enigmail.net#filterActionCopyDecrypt") {
+        hasCopyAction = i;
+        break;
+      }
+
+      if (action.customId == "enigmail@enigmail.net#filterActionMoveDecrypt") {
+        hasMoveAction = i;
+        if (i < gActionListOrdered.length - 1) {
+          forbidden = i;
+        }
+      }
+
+      if (action.type == nsMsgFilterAction.StopExecution &&
+          i == gActionListOrdered.length - 1 &&
+          forbidden == i - 1) {
+        // allow "stop execution" if it's the only action after move
+        forbidden = -1;
+      }
+    }
+
+    if (forbidden >= 0 || (hasMoveAction >= 0 && hasCopyAction > hasMoveAction)) {
+      document.getElementById("enigmailInfobar").setAttribute("style", "visibility: visible");
+      acceptButton.setAttribute("disabled", "true");
+    }
+    else {
+      document.getElementById("enigmailInfobar").setAttribute("style", "visibility: hidden");
+      acceptButton.setAttribute("disabled", "false");
+    }
+  }
+}
