29d3d9e0c2a8b922255c904863f8d335a5c1f4ce
Ola Bini <ola.bini@gmail.com>
2015-06-15 10:03:07 -0500

    Small helper for running the tests.
    Contribution from Fan Jiang, Iván Pazmiño and Ola Bini.

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+Xvfb :99 >/dev/null 2>&1 &
+export DISPLAY=:99
+export PL_PATH=`which perl`
+export TB_PATH=${TB_PATH:-`which thunderbird`}
+
+if [ "$#" -eq 0 ]; then
+  util/run-tests.py
+else
+  util/run-tests.py $@
+fi
+
+RESULT=$?
+
+killall Xvfb
+exit $RESULT
