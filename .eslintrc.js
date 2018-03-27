a8865698bba87811acf71a9f75ed1540d7cee4f3
Patrick Brunschwig <patrick@enigmail.net>
2018-03-27 18:39:12 +0200

    updated to accept global components

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,5 +66,12 @@ module.exports = {
     "browser": true,
     "node": true,
   },
-  "extends": "eslint:recommended"
+  "extends": "eslint:recommended",
+  "globals": {
+     "ChromeUtils": true,
+     "Components": true,
+     "Cc": true,
+     "Cu": true,
+     "Ci": true
+  }
 };
