eb14ad601999574716ff5f4932e29ecc290b44c6
Patrick Brunschwig <patrick@enigmail.net>
2018-10-21 16:30:27 +0200

    fixed error with "editable" being removed from TB 63

--- ui/content/enigmailKeyserverDlg.js
+++ ui/content/enigmailKeyserverDlg.js
@@ -10,15 +10,9 @@
 const Cu = Components.utils;
 const Ci = Components.interfaces;
 
-const {
-  EnigmailLocale
-} = Cu.import("chrome://enigmail/content/modules/locale.jsm");
-const {
-  EnigmailPrefs
-} = Cu.import("chrome://enigmail/content/modules/prefs.jsm");
-const {
-  EnigmailDialog
-} = Cu.import("chrome://enigmail/content/modules/dialog.jsm");
+const {EnigmailLocale} = Cu.import("chrome://enigmail/content/modules/locale.jsm");
+const {EnigmailPrefs} = Cu.import("chrome://enigmail/content/modules/prefs.jsm");
+const {EnigmailDialog} = Cu.import("chrome://enigmail/content/modules/dialog.jsm");
 
 
 function onLoad() {
@@ -27,7 +21,7 @@ function onLoad() {
   let searchCollapser = document.getElementById("searchCollapser");
   let keyText;
 
-  if (typeof(window.arguments[0].keyId) == "string") {
+  if (typeof (window.arguments[0].keyId) == "string") {
     var keyId = window.arguments[0].keyId;
     if (window.arguments[0].upload) {
       keyText = EnigmailLocale.getString("uploadKey", keyId);
@@ -48,44 +42,50 @@ function onLoad() {
 
   var keyservers = EnigmailPrefs.getPref("keyserver").split(/[ ,;]/g);
   var menulist = document.getElementById("selectedServer");
-  var selected;
 
   for (var i = 0; i < keyservers.length; i++) {
     if (keyservers[i].length > 0 &&
       (!window.arguments[0].upload ||
-        keyservers[i].slice(0, 10) != "keybase://")) {
+      keyservers[i].slice(0, 10) !== "keybase://")) {
       menulist.appendItem(keyservers[i]);
-
-      if (selected === undefined) {
-        selected = keyservers[i];
-      }
     }
   }
-  document.getElementById("selectedServer").value = selected;
+
+  menulist.selectedIndex = 0;
+  onSelectServer(menulist);
 }
 
 function onAccept() {
+  let srvName = document.getElementById("enteredServerName");
   let menulist = document.getElementById("selectedServer");
-  window.arguments[1].value = menulist.value;
-  if (typeof(window.arguments[0].keyId) != "string") {
+  let srv = srvName.value;
+  window.arguments[1].value = srv;
+  if (typeof (window.arguments[0].keyId) !== "string") {
     window.arguments[1].email = document.getElementById("email").value;
     if (!window.arguments[1].email) {
       EnigmailDialog.alert(window, EnigmailLocale.getString("noEmailProvided"));
       return false;
     }
   }
-  var selected = menulist.selectedIndex;
 
-  if (selected !== 0) {
-    let servers = [menulist.value];
-    let nodes = menulist.menupopup.getElementsByTagName('menuitem');
-    for (let i = 0, e = nodes.length; i < e; ++i) {
-      if (i == selected) {
-        continue;
-      }
-      servers.push(nodes[i].label);
+  let servers = [srv];
+  let nodes = menulist.menupopup.getElementsByTagName('menuitem');
+  for (let i = 0, e = nodes.length; i < e; ++i) {
+    if (nodes[i].label == srv) {
+      continue;
     }
-    EnigmailPrefs.setPref("keyserver", servers.join(', '));
+    servers.push(nodes[i].label);
   }
+  EnigmailPrefs.setPref("keyserver", servers.join(', '));
+
   return true;
+}
+
+
+function onSelectServer(menuList) {
+  let srv = menuList.selectedItem;
+  if (srv) {
+    let srvName = document.getElementById("enteredServerName");
+    srvName.value = srv.label;
+  }
 }
