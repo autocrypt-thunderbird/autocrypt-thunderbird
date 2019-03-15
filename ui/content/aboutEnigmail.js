b67c6497d51f077c7228ea9e92ca0e7b63dd5d42
Patrick Brunschwig <patrick@enigmail.net>
2019-03-15 17:59:46 +0100

    - improved update text - added button to check for upgrades of GnuPG

---
+++ ui/content/aboutEnigmail.js
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const EnigmailLocalizeHtml = ChromeUtils.import("chrome://enigmail/content/modules/localizeHtml.jsm").EnigmailLocalizeHtml;
+const EnigmailWindows = ChromeUtils.import("chrome://enigmail/content/modules/windows.jsm").EnigmailWindows;
+const EnigmailGnuPGUpdate = ChromeUtils.import("chrome://enigmail/content/modules/gnupgUpdate.jsm").EnigmailGnuPGUpdate;
+
+function onload() {
+  EnigmailLocalizeHtml.onPageLoad(document);
+
+  if (EnigmailGnuPGUpdate.isGnuPGUpdatable()) {
+    document.getElementById("checkGnupgUpdate").classList.remove("hidden");
+  }
+}
+
+function checkGnupgUpdate() {
+  EnigmailWindows.openGnuPGUpdate();
+}
