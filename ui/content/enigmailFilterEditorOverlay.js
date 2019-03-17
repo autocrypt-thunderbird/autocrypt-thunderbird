a5ad8b85eb8ae7b892d8c06a2f6bb4504752651c
Patrick Brunschwig <patrick@enigmail.net>
2019-03-17 18:05:14 +0100

    made filter editor work again with TB 66

--- ui/content/enigmailFilterEditorOverlay.js
+++ ui/content/enigmailFilterEditorOverlay.js
@@ -8,8 +8,10 @@
 
 "use strict";
 
-/* global gActionListOrdered: false, checkActionsReorder: true */
-/* global nsMsgFilterAction: false, gFilterActionStrings: false, gFilterActionList: true */
+/* global Services: false */
+/* global gActionListOrdered: false, checkActionsReorder: true, nsMsgFilterType: false   */
+/* global  gFilterActionList: true, gCustomActions: false, gFilterList: false */
+/* global gFilterType: false, gFilterBundle: false */
 
 var EnigmailTimer = ChromeUtils.import("chrome://enigmail/content/modules/timer.jsm").EnigmailTimer;
 
@@ -23,7 +25,7 @@ var EnigmailFilterEditor = {
 
       if ("filter" in args) {
         // editing a filter
-        this.reInitialize(args.filter);
+        this.overwriteActionTypes(false);
       }
     }
 
@@ -32,7 +34,7 @@ var EnigmailFilterEditor = {
 
     checkActionsReorder = function() {
       let r = self.enigmail_origCheckActionsReorder();
-      EnigmailTimer.setTimeout(EnigmailFilterEditor.checkMoveAction.bind(EnigmailFilterEditor), 0);
+      EnigmailTimer.setTimeout(EnigmailFilterEditor.checkMoveAction.bind(EnigmailFilterEditor), 50);
       return r;
     };
   },
@@ -44,23 +46,38 @@ var EnigmailFilterEditor = {
     EnigmailFilterEditor = undefined;
   },
 
-  reInitialize: function(filter) {
-    while (gFilterActionList.firstChild) {
-      gFilterActionList.removeChild(gFilterActionList.firstChild);
+  overwriteAction: function(element, resetValues) {
+    let targetType = "";
+
+    switch (element.value) {
+      case "enigmail@enigmail.net#filterActionCopyDecrypt":
+        targetType = "copymessage";
+        break;
+      case "enigmail@enigmail.net#filterActionMoveDecrypt":
+        targetType = "movemessage";
+        break;
+      case "enigmail@enigmail.net#filterActionEncrypt":
+        targetType = "forwardmessage";
+        break;
     }
 
-    let numActions = filter.actionCount;
-    for (let actionIndex = 0; actionIndex < numActions; actionIndex++) {
-      let filterAction = filter.getActionAt(actionIndex);
+    if (targetType.length > 0) {
+      let currType = element.mRuleActionType.nextSibling.getAttribute("type");
+      if (currType != targetType) {
+        element.mRuleActionType.nextSibling.setAttribute("type", targetType);
+        element.mRuleActionType.menulist.value = element.value;
+      }
+    }
 
-      var newActionRow = document.createElement('listitem');
-      newActionRow.setAttribute('initialActionIndex', actionIndex);
-      newActionRow.className = 'ruleaction';
-      gFilterActionList.appendChild(newActionRow);
-      newActionRow.setAttribute('value',
-        filterAction.type == nsMsgFilterAction.Custom ?
-        filterAction.customId : gFilterActionStrings[filterAction.type]);
-      newActionRow.setAttribute('onfocus', 'this.storeFocus();');
+    element.validateAction = this.validateAction;
+  },
+
+  overwriteActionTypes: function(resetValues) {
+    let count = gFilterActionList.itemCount;
+
+    for (let i = 0; i < count; i++) {
+      let e = gFilterActionList.getItemAtIndex(i);
+      this.overwriteAction(e, resetValues);
     }
   },
 
@@ -71,7 +88,7 @@ var EnigmailFilterEditor = {
     let hasCopyAction = -1;
     let hasMoveAction = -1;
 
-    const nsMsgFilterAction = Components.interfaces.nsMsgFilterAction;
+    this.overwriteActionTypes(true);
 
     for (let i = 0; i < gActionListOrdered.length; i++) {
       let action = gActionListOrdered.queryElementAt(i, Components.interfaces.nsIMsgRuleAction);
@@ -87,7 +104,7 @@ var EnigmailFilterEditor = {
         }
       }
 
-      if (action.type == nsMsgFilterAction.StopExecution &&
+      if (action.type == Ci.nsMsgFilterAction.StopExecution &&
         i == gActionListOrdered.length - 1 &&
         forbidden == i - 1) {
         // allow "stop execution" if it's the only action after move
@@ -98,11 +115,81 @@ var EnigmailFilterEditor = {
     if (forbidden >= 0 || (hasMoveAction >= 0 && hasCopyAction > hasMoveAction)) {
       document.getElementById("enigmailInfobar").removeAttribute("hidden");
       acceptButton.setAttribute("disabled", "true");
-    }
-    else {
+    } else {
       document.getElementById("enigmailInfobar").setAttribute("hidden", "true");
       acceptButton.setAttribute("disabled", "false");
     }
+  },
+
+  /**
+   * Taken from FilterEditor.js; adjusted for Enigmail custom actions  
+   */
+  validateAction: function() {
+    // returns true if this row represents a valid filter action and false otherwise.
+    // This routine also prompts the user.
+    ChromeUtils.import("resource:///modules/MailUtils.jsm", this);
+    let filterActionString = this.getAttribute('value');
+    let actionTarget = document.getAnonymousNodes(this)[1];
+    let actionTargetLabel = actionTarget.ruleactiontargetElement &&
+      actionTarget.ruleactiontargetElement.childNodes[0].value;
+    let errorString, customError, msgFolder, actionItem;
+
+    switch (filterActionString) {
+      case "movemessage":
+      case "copymessage":
+        msgFolder = actionTargetLabel ?
+          this.MailUtils.getOrCreateFolder(actionTargetLabel) : null;
+        if (!msgFolder || !msgFolder.canFileMessages)
+          errorString = "mustSelectFolder";
+        break;
+      case "forwardmessage":
+        if (actionTargetLabel.length < 3 ||
+          actionTargetLabel.indexOf('@') < 1)
+          errorString = "enterValidEmailAddress";
+        break;
+      case "replytomessage":
+        if (!actionTarget.ruleactiontargetElement.childNodes[0].selectedItem)
+          errorString = "pickTemplateToReplyWith";
+        break;
+      case "enigmail@enigmail.net#filterActionEncrypt":
+        actionTarget.ruleactiontargetElement.childNodes[0].value = actionTarget.childNodes[0].childNodes[0].inputField.value;
+        actionTargetLabel = actionTarget.ruleactiontargetElement.childNodes[0].value;
+        /* eslint no-fallthrough: 0 */
+        // no break here
+      case "enigmail@enigmail.net#filterActionCopyDecrypt":
+      case "enigmail@enigmail.net#filterActionMoveDecrypt":
+        for (let i = 0; i < gCustomActions.length; i++)
+          if (gCustomActions[i].id == filterActionString) {
+            customError =
+              gCustomActions[i].validateActionValue(
+                actionTargetLabel,
+                gFilterList.folder, gFilterType);
+            break;
+          }
+        break;
+      default:
+        // some custom actions have no action value node
+        if (!document.getAnonymousNodes(actionTarget))
+          return true;
+        // locate the correct custom action, and check validity
+        for (let i = 0; i < gCustomActions.length; i++)
+          if (gCustomActions[i].id == filterActionString) {
+            customError =
+              gCustomActions[i].validateActionValue(
+                actionTargetLabel,
+                gFilterList.folder, gFilterType);
+            break;
+          }
+        break;
+    }
+
+    errorString = errorString ?
+      gFilterBundle.getString(errorString) :
+      customError;
+    if (errorString)
+      Services.prompt.alert(window, null, errorString);
+
+    return !errorString;
   }
 };
 
