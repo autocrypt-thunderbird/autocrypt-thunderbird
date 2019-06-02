c1f425609f749055a083cfad1cac74854c57ae15
Patrick Brunschwig <patrick@enigmail.net>
2019-06-02 15:54:08 +0200

    don't remove head when replying

--- ui/content/enigmailMsgComposeOverlay.js
+++ ui/content/enigmailMsgComposeOverlay.js
@@ -4851,7 +4851,7 @@ Enigmail.msg = {
     var beginIndex = beginIndexObj.value;
     var endIndex = endIndexObj.value;
 
-    const head = "";
+    var head = docText.substr(0, beginIndex);
     var tail = docText.substr(endIndex + 1);
 
     var pgpBlock = docText.substr(beginIndex, endIndex - beginIndex + 1);
